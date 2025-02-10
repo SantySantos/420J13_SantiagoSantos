e98f0f8b50bfe5edf9a0a584d893c3cc89f63586
SantySantos <santysantos533@gmail.com>
2025-02-10 11:06:54 -0500

    Using ForEach loop
    For each loops and functions

--- HelloWord/HelloWord.cpp
+++ HelloWord/HelloWord.cpp
@@ -3,83 +3,92 @@
 #include <iostream>
 #include <string>
 #include <limits>
-
-
-//int getID(const string& message, const string& errorMessage, const string& acceptedMessage, int minNumber, int maxNumber);
+#include <format>
 //string User(const string& message, const string& errorMessage);
 
 using namespace std;
 
 struct Course {
+
 	string name;
 	float midterm;
 	float exam;
 	float project;
 };
 
+float getID(const string& message, const string& errorMessage, const string& acceptedMessage, int minNumber, int maxNumber);
+Course createCourse(string name, float midterm, float project, float exam);
+
 int main()
 {
-	//num of courses
+    const int nbCourses = 3;
+    Course myCourses[nbCourses];
 
-	const int numCourses = 3;
+    string tempName;
+    float tempMidterm;
+    float tempProject;
+    float tempExam;
 
-	//creating the variable in the stack
-	Course courses[numCourses];
+    for (int i = 0; i < nbCourses; i++) 
+    {
+        cout << "Please enter your course name: ";
+        getline(cin, tempName);
 
-	//adding info to the courses
-	courses[0].name = "Game Engine";
-	courses[0].midterm = 30;
-	courses[0].project = 30;
-	courses[0].exam = 40;
+        tempMidterm = getID("Please enter your midterm score: ", "Please enter a number between 0 and 100", "Good number", 0, 100);
+        tempProject = getID("Please enter your project score: ", "Please enter a number between 0 and 100", "Good number", 0, 100);
+        tempExam = getID("Please enter your exam score: ", "Please enter a number between 0 and 100", "Good number", 0, 100);
+        myCourses[i] = createCourse(tempName, tempMidterm, tempProject, tempExam);
+    }
 
-	courses[1].name = "OOP";
-	courses[1].midterm = 25;
-	courses[1].project = 20;
-	courses[1].exam = 35;
-
-	courses[2].name = "French";
-	courses[2].midterm = 25;
-	courses[2].project = 15;
-	courses[2].exam = 30;
-
-	for (int i = 0; i < numCourses; i++) {
-
-		cout << "Course Name: " << courses[i].name << "\n"
-			<< "Midterm: " << courses[i].midterm << "\n"
-			<< "Project: " << courses[i].project << "\n"
-			<< "Final Exam: " << courses[i].exam << "\n"
-			<< "Total:" << courses[i].midterm + courses[i].project + courses[i].exam << "\n"
-			<< "\n";
-	}
-	
-
+    for (Course myCourse : myCourses) {
+        cout << "\n"
+            << "your course name is: " << myCourse.name << "\n"
+            << "your midterm score is: " << myCourse.midterm << "\n"
+            << "your project score is: " << myCourse.project << "\n"
+            << "your exam score is: " << myCourse.exam << endl;
+    }
+   
 	return 0;
 }
-//
-//int getID(const string& message, const string& errorMessage, const string& acceptedMessage,int minNumber, int maxNumber)
-//{
-//    int number;
-//    while (true) {
-//        cout << message;
-//        cin >> number;
-//
-//        if (cin.fail()) 
-//        {
-//            cin.clear();
-//            cin.ignore(numeric_limits<streamsize>::max(), '\n');
-//            cout << errorMessage << endl;
-//            continue;
-//            
-//        }
-//        if (number < minNumber || number > maxNumber) 
-//        {
-//            cout << errorMessage << endl;
-//            continue;
-//        }
-//        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-//        return number;
-//    }
-//}
+
+
+Course createCourse(string name, float midterm, float project, float exam) 
+{
+    //creating the variable in the stack
+    Course myCourse;
+    myCourse.name = name;
+    myCourse.midterm = midterm;
+    myCourse.project = project;
+    myCourse.exam = exam;
+
+  
+
+    return myCourse;
+}
+float getID(const string& message, const string& errorMessage, const string& acceptedMessage, int minNumber, int maxNumber)
+{
+    float number;
+    while (true) {
+        cout << message;
+        cin >> number;
+
+        if (cin.fail()) 
+        {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << errorMessage << endl;
+            continue;
+            
+        }
+        if (number < minNumber || number > maxNumber) 
+        {
+            cout << errorMessage << endl;
+            continue;
+        }
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        return number;
+    }
+}
 //
 //string User(const string& message, const string& errorMessage)
 //{
